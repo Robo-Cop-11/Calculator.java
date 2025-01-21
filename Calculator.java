91a2d6e36cf2ee52e439c98e0176e4d9356d9f56
Robo-Cop-11 <itsleooo20@gmail.com>
2025-01-21 23:27:19 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,16 +1,20 @@
 public class Calculator {
+    // Method to add two numbers
     public int addition(int[] numbers) {
         return numbers[0] + numbers[1];
     }
 
+    // Method to subtract two numbers
     public int subtraction(int[] numbers) {
         return numbers[0] - numbers[1];
     }
 
+    // Method to multiply two numbers
     public int multiplication(int[] numbers) {
         return numbers[0] * numbers[1];
     }
 
+    // Method to divide two numbers
     public int division(int[] numbers) {
         if (numbers[1] == 0) {
             throw new ArithmeticException("Division by zero is not allowed.");
@@ -18,6 +22,7 @@ public class Calculator {
         return numbers[0] / numbers[1];
     }
 
+    // Method to display the Fibonacci sequence up to a given number of terms
     public void fibonacci(int terms) {
         int a = 0, b = 1;
         System.out.print("Fibonacci Sequence: " + a + " " + b);
@@ -30,6 +35,7 @@ public class Calculator {
         System.out.println();
     }
 
+    // Method to calculate the sum of an array
     public int sumOfArray(int[] array) {
         int sum = 0;
         for (int num : array) {
@@ -38,6 +44,7 @@ public class Calculator {
         return sum;
     }
 
+    // Method to calculate the mean of an array
     public double meanOfArray(int[] array) {
         return (double) sumOfArray(array) / array.length;
     }
